be7395c0c28d3b7f095a891569438d6228ca8cc1
student <student@192.168.52.66>
2018-04-23 12:35:38 +0800

    -

--- src/main/java/ouhk/comps380f/controller/TicketUserController.java
+++ src/main/java/ouhk/comps380f/controller/TicketUserController.java
@@ -1,9 +1,19 @@
 package ouhk.comps380f.controller;
 
 import java.io.IOException;
+
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +22,19 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 import ouhk.comps380f.dao.TicketUserRepository;
 import ouhk.comps380f.model.TicketUser;
+import ouhk.comps380f.validator.UserValidator;
 
 @Controller
 @RequestMapping("user")
 public class TicketUserController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
     TicketUserRepository ticketUserRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserValidator userValidator;
 
     @RequestMapping(value = {"", "list"}, method = RequestMethod.GET)
     public String list(ModelMap model) {
@@ -28,8 +44,14 @@ public class TicketUserController {
 
     public static class Form {
 
+        @NotEmpty(message = "Please enter your user name.")
         private String username;
+        @NotEmpty(message = "Please enter your password.")
+        @Size(min = 6, max = 15,
+                message = "Your password must be between {min} and {max} characters.")
         private String password;
+        private String confirm_password;
+        @NotEmpty(message = "Please select at least one role.")
         private String[] roles;
 
         public String getUsername() {
@@ -48,6 +70,14 @@ public class TicketUserController {
             this.password = password;
         }
 
+        public String getConfirm_password() {
+            return confirm_password;
+        }
+
+        public void setConfirm_password(String confirm_password) {
+            this.confirm_password = confirm_password;
+        }
+
         public String[] getRoles() {
             return roles;
         }
@@ -60,22 +90,29 @@ public class TicketUserController {
 
     @RequestMapping(value = "create", method = RequestMethod.GET)
     public ModelAndView create() {
+
         return new ModelAndView("addUser", "ticketUser", new Form());
     }
 
-    @RequestMapping(value = "create", method = RequestMethod.POST)
-    public View create(Form form) throws IOException {
+    public String create(@ModelAttribute("ticketUser") @Valid Form form,
+            BindingResult result) throws IOException {
+        userValidator.validate(form, result);
+        if (result.hasErrors()) {
+            return "addUser";
+        }
         TicketUser user = new TicketUser(form.getUsername(),
-                form.getPassword(),
+                passwordEncoder.encode(form.getPassword()),
                 form.getRoles()
         );
         ticketUserRepo.save(user);
-        return new RedirectView("/user/list", true);
+        logger.info("User " + form.getUsername() + " created.");
+        return "redirect:/user/list";
     }
 
     @RequestMapping(value = "delete/{username}", method = RequestMethod.GET)
     public View deleteTicket(@PathVariable("username") String username) {
         ticketUserRepo.delete(ticketUserRepo.findOne(username));
+        logger.info("User " + username + " deleted.");
         return new RedirectView("/user/list", true);
     }
 
